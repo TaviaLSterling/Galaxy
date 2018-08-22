213eb70d2600ed073ad072a440484391ce1e16d2
Tavia Sterling <TaviaSterling@outlook.com>
2018-08-22 14:20:21 -0600

    routes setup

---
+++ models/moon.js
@@ -0,0 +1,44 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let ObjectId = Schema.Types.ObjectId
+let schemaName = 'Moon'
+
+
+let schema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: 'No Description Choose your own fate!'
+  },
+  color: {
+    type: String,
+    enum: [
+      'red',
+      'yellow',
+      'blue',
+      'orange',
+      'white',
+      'black',
+      'grey'
+    ],
+    required: true
+  },
+
+  galaxyId: {
+    type: ObjectId,
+    ref: 'Galaxy',
+    required: true
+  },
+  planetId: {
+      type:ObjectId,
+      ref: 'Planet',
+      required: true
+  }
+
+
+})
+
+module.exports = mongoose.model(schemaName, schema)
